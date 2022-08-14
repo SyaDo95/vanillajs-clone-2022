060c9ab58712410d534e39bc957cf70e3eef40bb
SyaDo95 <tla1503@naver.com>
2022-08-14 22:31:05 +0900

    useing css

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const loginInput = document.querySelector("#login-form input")
-const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     console.log(username);
-    
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
