ac736a05066666e33609452f4ffa01632bcbd2bb
SyaDo95 <tla1503@naver.com>
2022-08-14 21:40:30 +0900

    prevent event

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const loginInput = document.querySelector("#login-form input")
-const loginButton = document.querySelector("#login-form button")
+const loginForm = document.querySelector("#login-form")
 
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
-    if(username === ""){
-        alert("Please write your name");
-    }else if (username.length > 15) {
-        alert("Your name is too long.")
-    }
 
     console.log(username);
     
 }
 
-loginButton.addEventListener("click",onLoginBtnClick)
+loginForm.addEventListener("submit",onLoginSubmit);
